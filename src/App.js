7790eef0945ba177faa3044ede4e666bb50373c4
CarolEnache <enache.carol@gmail.com>
2020-07-24 11:13:19 +0100

    Fetch the API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useFetch } from './hooks';
 
 function App() {
+  const { response } = useFetch(
+    'https://www.reddit.com/r/wallpapers/top.json?t=all'
+  );
+
+  console.log(response);
+
   return (
     <div className='App' data-testid='app-container'>
       <h1>hello world</h1>
